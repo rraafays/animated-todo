9e3f720861972626925679328be96498983fd1ae
rraafays <rraf@tuta.io>
2022-06-28 16:16:44 +0100

    update: proof of concept using colormode value to alternate colours of other components

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
-import { Text, Box, Center, VStack, themeTools, useTheme, useColorMode, useColorModeValue } from 'native-base'
+import { Text, Box, Center, VStack, useColorModeValue } from 'native-base'
 import ThemeToggle from '../components/theme-toggle'
 
 export default function MainScreen() {
   return (
     <Center _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} px={4} flex={1}>
       <VStack space={5} alignItems="center">
-        <Box>
+        <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
           <Text>hello</Text>
         </Box>
         <ThemeToggle />
